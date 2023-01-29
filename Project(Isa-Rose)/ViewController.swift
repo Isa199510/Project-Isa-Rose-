c5e413508aeca0a57cb9371556efd8521d9acb69
Isa <isa.arbuhov@mail.ru>
2023-01-29 21:10:45 +0300

    Initial Commit

---
+++ Project(Isa-Rose)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project(Isa-Rose)
+//
+//  Created by Иса on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
